aab63c1d7a7b2e61ae706759a7cfbc6483615383
Asahel Enock Kigen <kirwaasahel@gmail.com>
2022-08-07 23:47:55 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; //Importing react router dom to facilate routing in the app
+import NavbarSection from './components/Navbar'; //Importing Navbar component to be accessed by all the apps
+import Footer from './components/Footer'; //Importing Footer component to be accessed by all the apps
+import { useRouter } from './utils/routes'; //Importing array of routes
 
 function App() {
+
+  //Assining Array of useRouter to routes
+  const routes = useRouter();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <NavbarSection />
+          <Routes>
+            {routes &&
+              routes.map((r) => (
+                <Route exact key={r.path} path={r.path} element={r.element} />
+              ))}
+          </Routes>u
+        <Footer />
+      </Router>
+
+
     </div>
   );
 }
